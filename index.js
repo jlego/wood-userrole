04d169de44a6fd6d6d8c36907784c58f49791e3b
yuronghui <yuronghui@singworld.net>
2018-11-22 17:52:20 +0800

    no message

---
+++ index.js
@@ -0,0 +1,40 @@
+/**
+ * Wood Plugin Module.
+ * user role
+ * by jlego on 2018-11-21
+ */
+const { Util } = require('wood-util')();
+
+module.exports = (app = {}, config = {}) => {
+  app.CheckRole = async function(req, res, next) {
+    if(req.User){
+      let RedisCaches = new app.Plugin('redis').Redis(`userrole.${req.User.role}`, config.redis),
+        urlArr = req.url.split('/'),
+        actionType = urlArr.pop(),
+        key = urlArr.join('_'),
+        hasVal = await RedisCaches.isHaseExist(key);
+      if(hasVal){
+        let actionArr = await RedisCaches.getHaseValue(key),
+          actionMap = {
+            list:   "read", 
+            detail: "read", 
+            create: "create", 
+            add:    "create", 
+            update: "update", 
+            remove: "remove",
+            delete: "remove",
+          },
+          action = actionMap[actionType];
+        if(actionArr.includes(action)){
+          next();
+        }else{
+          if(res.print) res.print(Util.error(`用户没有[${action}]权限`));
+          return false;
+        }
+      }
+    }
+    if(res.print) res.print(Util.error('找不到当前用户信息'));
+    return false;
+  };
+  return app;
+}
